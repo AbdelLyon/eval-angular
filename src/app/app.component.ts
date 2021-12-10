16ea5d3f6151c72153e48014f2a97d0ee87d0b63
majax <abdelmajidahamiane@gmail.com>
2021-12-10 11:17:28 +0100

    Exercice4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'evalAngular';
+export class AppComponent implements OnInit {
+  public stat!: Statistique;
+  public stats: Statistique[] = [];
+
+
+  ngOnInit() {
+    this.stats = [new Statistique('fa1f5f40-be3b-11eb-91ec-7f5875ecfb46', 'Démographie en France', '60M'),
+    new Statistique('fa1f5f40-be3b-11eb-91ec-7f5875ecfb47', 'Démographie Du Maroc', '43M')];
+    console.log(this.stats);
+
+
+  }
+
 }
