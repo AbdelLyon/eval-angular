453f165350554f92a1beed6e05a7b9e47611fab2
majax <abdelmajidahamiane@gmail.com>
2021-12-10 11:48:11 +0100

    Exercice5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Statistique } from './models/statistique';
 
 @Component({
@@ -11,12 +11,12 @@ export class AppComponent implements OnInit {
   public stats: Statistique[] = [];
 
 
+
+
+
   ngOnInit() {
     this.stats = [new Statistique('fa1f5f40-be3b-11eb-91ec-7f5875ecfb46', 'Démographie en France', '60M'),
     new Statistique('fa1f5f40-be3b-11eb-91ec-7f5875ecfb47', 'Démographie Du Maroc', '43M')];
-    console.log(this.stats);
-
-
   }
 
 }
---
+++ src/app/statistique/statistique.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Statistique } from '../models/statistique';
+
+@Component({
+  selector: 'app-statistique',
+  templateUrl: './statistique.component.html',
+  styleUrls: ['./statistique.component.css']
+})
+export class StatistiqueComponent implements OnInit {
+
+  @Input() public stats!: Statistique;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
