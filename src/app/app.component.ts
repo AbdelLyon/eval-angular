752db1257fe8fec9339adba0eeeafdc21c8b5923
majax <abdelmajidahamiane@gmail.com>
2021-12-10 11:54:21 +0100

    Exercice6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,20 @@ export class AppComponent implements OnInit {
   public stats: Statistique[] = [];
 
 
+  constructor() {
+    setTimeout(() => {
+      this.stats.push(new Statistique('fa1f5f40-be3b-11eb-91ec-7f5875ecfb48', 'Altitude de la france', '9.82822M'))
+    }, 2000)
+  }
+
+
 
 
 
   ngOnInit() {
+
+
+
     this.stats = [new Statistique('fa1f5f40-be3b-11eb-91ec-7f5875ecfb46', 'Démographie en France', '60M'),
     new Statistique('fa1f5f40-be3b-11eb-91ec-7f5875ecfb47', 'Démographie Du Maroc', '43M')];
   }
